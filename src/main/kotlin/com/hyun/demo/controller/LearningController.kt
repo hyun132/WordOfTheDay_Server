c8e6dfac010e73f51146760259c843b277adb76d
hyun <rlaalgus132@naver.com>
2025-06-03 22:28:29 +0900

    잘못 설정한 어노테이션 수정 (Controller -> RestController)

--- src/main/kotlin/com/hyun/demo/controller/LearningController.kt
+++ src/main/kotlin/com/hyun/demo/controller/LearningController.kt
@@ -5,16 +5,16 @@ import com.hyun.demo.dto.response.AnswerCheckResponse
 import com.hyun.demo.service.LearningService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class LearningController(
     val service: LearningService
 ) {
     @PostMapping("/learning/answer")
-    fun checkAnswer(@RequestBody @Valid request : AnswerSubmitRequest): ResponseEntity<AnswerCheckResponse> {
+    fun checkAnswer(@RequestBody @Valid request: AnswerSubmitRequest): ResponseEntity<AnswerCheckResponse> {
         val result = service.checkAnswer(request)
         return ResponseEntity.ok(result)
     }
