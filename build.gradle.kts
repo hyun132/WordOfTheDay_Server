4378804b5c9b4aa42ab5ec6bacdf13ceb737a9c6
hyun <rlaalgus132@naver.com>
2025-08-09 22:13:15 +0900

    springboot버전 낮춤

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
     kotlin("plugin.noarg") version "1.9.25"
-    id("org.springframework.boot") version "3.4.3"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
